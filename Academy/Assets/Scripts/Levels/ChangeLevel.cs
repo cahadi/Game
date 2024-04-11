f81eecd2fbc3d891153b36e2244c451b1dc29735
cahadi <ap357775462534@gmail.com>
2024-04-11 13:59:38 +1000

    demo finished

---
+++ Academy/Assets/Scripts/Levels/ChangeLevel.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour
+{
+    [SerializeField] int _numScene;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        SceneManager.LoadScene(_numScene);
+    }
+}
